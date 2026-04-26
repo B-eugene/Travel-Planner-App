f474f43a71c1e2ad0a838da50b52a009a24a6dc1
B-eugene <oseieugene254@gmail.com>
2026-04-26 16:37:40 +0100

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Urban Mobility Hub</h1>
+      <p>Use this app to plan your travel efficiently.</p>
+    </div>
+  );
+}
+
+export default Home;
